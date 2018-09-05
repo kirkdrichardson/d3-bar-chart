c3b7d42eb3562dc31fae4b73884e6283fa943d04
Kirk <kirk.douglas.richardson@gmail.com>
2018-09-04 23:24:19 -0600

    added style

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ export default class App extends Component {
   render() {
     return (
       <AppContainer>
+        <BackgroundMask />
         <ChartContainer>
-          <h2>US GDP</h2>
+          <h1>U.S. GDP</h1>
+          <p>{'figures are in the billions'}</p>
           <BarChart />
         </ChartContainer>
       </AppContainer>
@@ -18,19 +20,44 @@ export default class App extends Component {
 
 
 const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow-y: scroll;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fafafa;
+`;
+
+const ChartContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 40px;
+
+  h1 {
+    margin-bottom: 0;
+  }
+
+  p {
+    margin-top: 10px;
+  }
+
+  img {
+    height: 50px;
+    width: ${Math.round(1.9 * 50)}px;
+  }
+`;
+
+const BackgroundMask = styled.div`
   position: absolute;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  overflow-y: scroll;
   background: #222;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const ChartContainer = styled.div`
-  padding: 20px 40px;
-  background: gray;
+  opacity: 0.8;
+  z-index: -99;
 `;
